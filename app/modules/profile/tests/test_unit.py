cb97715cb78e8dd13b89ff13f54e2a157fc9f7c1
celiasuaco <114919457+celiasuaco@users.noreply.github.com>
2025-10-14 10:00:42 +0200

    Update test_unit.py

--- app/modules/profile/tests/test_unit.py
+++ app/modules/profile/tests/test_unit.py
@@ -36,3 +36,16 @@ def test_edit_profile_page_get(test_client):
     assert b"Edit profile" in response.data, "The expected content is not present on the page"
 
     logout(test_client)
+
+def test_list_empty_notepad_get(test_client):
+    """
+    Tests access to the empty notepad list via GET request.
+    """
+    login_response = login(test_client, "user@example.com", "test1234")
+    assert login_response.status_code == 200, "Login was unsuccessful."
+
+    response = test_client.get("/notepad")
+    assert response.status_code == 200, "The notepad page could not be accessed."
+    assert b"You have no notepads." in response.data, "The expected content is not present on the page"
+
+    logout(test_client)
